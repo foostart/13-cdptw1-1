c7ce2f9b202bed2bead4e40cf26c893e0f8468db
foostart.com <foostart.com@gmail.com>
2019-09-17 15:57:11 +0700

    f

---
+++ 1226/1226-content.php
@@ -0,0 +1,45 @@
+<?php
+include '../config.php';
+
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), $sala);
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+    <div class="type-1226">
+        <!--CONTAINER-->
+        <div class="container line">
+            <!--ROW-->
+            <div class="row">
+                <!--MAP-->
+                <div class="col-md-9 col-xs-12 dis-contact">
+                    <div class="maps">
+                        <iframe  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3918.9808979555014!2d106.70530451477175!3d10.812773492296875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x317528965ce85f51%3A0xdc955af39102151a!2zNDMgxJDGsOG7nW5nIHPhu5EgNg!5e0!3m2!1sen!2s!4v1525610301467" width="800" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <!--/MAP-->
+                <!--CONTACT-->
+                <div class="col-md-3 col-xs-12">
+                    <!--INFO-->
+                    <div class="info">
+                        <h1>Liên hệ: </h1>
+                        <h4><i class="fa fa-home" aria-hidden="true">&nbsp; &nbsp;&nbsp;&nbsp;|&nbsp; &nbsp;Công ty cổ phần xây dựng Sài Gòn KT-ROYAL</i></h4>
+                        <h2><i class="fa fa-map-marker" aria-hidden="true">&nbsp; &nbsp;&nbsp;&nbsp;|&nbsp; &nbsp;43 đường số 6, Đinh Bộ Lĩnh, P.26, Q.Bình Thạnh, Tp.HCM</i></h2>
+                        <h2><i class="fa fa-mobile" aria-hidden="true">&nbsp;&nbsp;&nbsp; |&nbsp;0982.956.427 </i></h2>
+                        <h5><i class="fa fa-envelope-o" aria-hidden="true">&nbsp;&nbsp;&nbsp; |&nbsp;thaikienkts@gmail.com</i></h5>
+                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                    </div>
+                    <!--/INFO-->
+                </div>
+                <!--/CONTACT-->
+            </div>
+            <!--/ROW-->
+        </div>
+        <!--/CONTAINER-->
+    </div>
---
+++ 1227/1227-content.php
@@ -0,0 +1,176 @@
+<?php
+include '../config.php';
+
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), $sala);
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+?>
+<div class="type-1227">
+    <!--HEADER-->
+    <div class="header">
+        <img class="image-s" src="<?php echo $url_path ?>/images/bg-header-6.jpg">
+        <div class="text-center-image">
+            <div class="text-1">
+                <span>KT-ROYAL</span>
+            </div>
+            <hr class="hr-size-color">
+            <div class="text-2">Cung cấp dịch vụ</div>
+        </div>
+    </div>
+    <!--/HEADER-->
+    <!--CONTAINER-->
+    <div class="container line">
+        <!--PD-HEARDER-->
+        <div class="pading-header">
+            <div class="text-3">
+                <span>Sứ mạng</span>
+                <span class="color">KT-ROYAL</span>
+            </div>
+            <hr class="hr-size-color2">
+            <div class="text-5">
+                <span>Công trình của bạn là niềm hãnh diện cho đội ngủ KT-ROYAL chúng tôi</span>
+            </div>
+        </div>
+        <!--/PD-HEARDER-->
+        <div class="clear"></div>
+        <!--WOR-->
+        <div class="row">
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-stats fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Tổng thầu xây dựng</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-equalizer fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Thiết kế và thi công</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-tower fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Nội thất</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-tasks fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Hạ tầng</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-flash fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Cơ điện</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+
+            <!--COL-->
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <!--TEXT-->
+                <div class="center-text">
+                    <div class="pading-top">
+                        <i class="border glyphicon glyphicon-fullscreen fa-3x " aria-hidden="true"></i>
+                    </div>
+                    <div class="text-6">
+                        <span>Dịch vụ khác</span>
+                    </div>
+                    <div class="hr-size-color3"></div>
+                    <div class="text-7">
+                        <span>The standard chunk of Lorem Ipsum </span>
+                    </div>
+                    <div class="text-7">
+                        <span>chunk of Lorem Ipsum </span>
+                    </div>
+                </div>
+                <!--/TEXT-->
+            </div>
+            <!--/COL-->
+
+        </div>
+        <!--/ROW-->
+    </div>
+    <!--/CONTAINER-->
+</div>
